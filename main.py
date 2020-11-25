5eeeb892fe2e3abdabe6e016f1df47746208b74f
Silja Walenius <46867889+siljawalenius@users.noreply.github.com>
2020-11-25 08:54:07 -0500

    Update main.py
    adding notes + commenting out test

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ def send_text(cell_num, text):
     os.system('osascript send.scpt {} "{}"'.format(cell_num, text))
 
 #test
-schedule.every().day.at("00:12").do(send_text,number,message)
+#schedule.every().day.at("00:12").do(send_text,number,message)
 
 #schedule a text every hour between 8 and 6
 #array of hours to send a text
+#I should have just use a range and convert to strings... but it was midnight... so...
 hours = ["08:00", "09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"]
 
 for hour in hours:
